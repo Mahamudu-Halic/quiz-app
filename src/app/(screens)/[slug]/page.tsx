ac83ab6bc1ed93f96ec7561c7b2bab6d075195cb
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2024-07-06 21:59:05 +0000

    created slug page for all questions to be displayed

---
+++ src/app/(screens)/[slug]/page.tsx
@@ -0,0 +1,80 @@
+"use client";
+import PageWrapper from "@/components/pageWrapper";
+import QuestionComponent from "@/components/question";
+import React, { useEffect, useState } from "react";
+import { Questions, Topics } from "../../../../constants";
+import QuizCompleted from "@/components/quizCompleted";
+import { QuestionsType, QuizCategory, Topic } from "../../../../types";
+import { useSearchParams } from "next/navigation";
+
+const QuizScreen = (props) => {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [completed, setCompleted] = useState(false);
+  const [score, setScore] = useState(0);
+
+  const [location, setLocation] = useState<string>("");
+  const [quizQuestions, setQuizQuestions] = useState<QuizCategory | null>(null);
+  const [topic, setTopic] = useState<Topic | null>(null);
+
+  const getTopic = () => {
+    const data = Topics.find(
+      (item) => item.topic.toLowerCase() === location.toLowerCase()
+    );
+    setTopic(data || null);
+  };
+
+  const getQuestions = () => {
+    setQuizQuestions(
+      (Questions as QuestionsType)[location as keyof QuestionsType]
+    );
+  };
+
+  useEffect(() => {
+    setLocation(props.params.slug);
+  }, []);
+
+  useEffect(() => {
+    location && getQuestions();
+    location && getTopic();
+  }, [location]);
+
+  const handleCompletion = () => {
+    setCompleted(true);
+  };
+
+  const handleScore = () => {
+    setScore(score + 1);
+  };
+
+  const handleNextQuestion = () => {
+    quizQuestions &&
+      currentQuestion < quizQuestions?.questions.length - 1 &&
+      setCurrentQuestion(currentQuestion + 1);
+  };
+
+  return (
+    quizQuestions && (
+      <PageWrapper>
+        {!completed ? (
+          <QuestionComponent
+            question={quizQuestions?.questions[currentQuestion]}
+            total={quizQuestions?.questions.length}
+            nextQuestion={handleNextQuestion}
+            handleScore={handleScore}
+            handleCompletion={handleCompletion}
+          />
+        ) : (
+          topic && (
+            <QuizCompleted
+              total={quizQuestions?.questions.length}
+              score={score}
+              topic={topic}
+            />
+          )
+        )}
+      </PageWrapper>
+    )
+  );
+};
+
+export default QuizScreen;
