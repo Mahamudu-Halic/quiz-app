d07ee912fea3c8b03fed51be0a1af821b03c16fd
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2024-07-07 07:16:28 +0000

    made some minor changes

--- src/components/welcome.tsx
+++ src/components/welcome.tsx
@@ -4,8 +4,8 @@ const WelcomeMessageComponent = () => {
   return (
     <div className="flex-1">
       <div className="mb-5 lg:mb-10">
-        <p className="md:text-4xl text-2xl">Welcome to the</p>
-        <h1 className="md:text-5xl text-3xl font-bold">Frontend Quiz!</h1>
+        <p className="md:text-4xl text-3xl">Welcome to the</p>
+        <h1 className="md:text-5xl text-4xl font-bold">Frontend Quiz!</h1>
       </div>
       <p className="text-sm mb-5">Pick a subject to get started.</p>
     </div>
